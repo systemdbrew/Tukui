21d4142b8f016923cf99b93fe17bcb9e8ded5c9f
Jonathan Bohbot <supra89t@gmail.com>
2026-08-26 15:25:44 -0400

    fix(retail): make unit health and power text secret-safe

--- Tukui/Core/Temp.lua
+++ Tukui/Core/Temp.lua
@@ -6,6 +6,70 @@ local T, C, L = unpack((select(2, ...)))
 
 local Temp = CreateFrame("Frame", nil, UIParent)
 
+-- Midnight 12.1 can return secret values from unit health and power APIs.
+-- Tukui's legacy formatting performs Lua comparisons/arithmetic on those
+-- values, which is forbidden. Keep this compatibility layer temporary until
+-- the unit-frame text paths are ported to the modern Retail APIs.
+if T.Midnight then
+	local UnitFrames = T["UnitFrames"]
+	local issecretvalue = issecretvalue
+	local TruncateWhenZero = C_StringUtil and C_StringUtil.TruncateWhenZero
+
+	local function SecretSafeText(value)
+		if issecretvalue and issecretvalue(value) then
+			return TruncateWhenZero and TruncateWhenZero(value) or ""
+		end
+
+		return value
+	end
+
+	local LegacyShortValue = UnitFrames.ShortValue
+	UnitFrames.ShortValue = function(self)
+		if issecretvalue and issecretvalue(self) then
+			return TruncateWhenZero and TruncateWhenZero(self) or ""
+		end
+
+		return LegacyShortValue(self)
+	end
+
+	UnitFrames.PostUpdatePower = function(self, unit, current, min, max)
+		if not self.Value then
+			return
+		end
+
+		local pType, pToken = UnitPowerType(unit)
+		local Color = T.Colors.power[pToken]
+
+		if Color then
+			self.Value:SetTextColor(Color[1], Color[2], Color[3])
+		end
+
+		if UnitIsDead(unit) or UnitIsGhost(unit) then
+			self.Value:SetText("")
+		else
+			self.Value:SetText(SecretSafeText(current) or "")
+		end
+	end
+
+	UnitFrames.PostUpdateHealth = function(self, unit, min, max)
+		if not self.Value then
+			return
+		end
+
+		if not UnitIsConnected(unit) then
+			self.Value:SetText("|cffD7BEA5" .. FRIENDS_LIST_OFFLINE .. "|r")
+		elseif UnitIsDead(unit) then
+			self.Value:SetText("|cffD7BEA5" .. DEAD .. "|r")
+		elseif UnitIsGhost(unit) then
+			self.Value:SetText("|cffD7BEA5" .. L.UnitFrames.Ghost .. "|r")
+		else
+			-- Do not compare, divide, or color-gradient secret health values.
+			self.Value:SetTextColor(0.33, 0.59, 0.33)
+			self.Value:SetText(SecretSafeText(min) or "")
+		end
+	end
+end
+
 -- TEMP for bugs fixes
 function Temp:Enable()
 	if T.Retail then
