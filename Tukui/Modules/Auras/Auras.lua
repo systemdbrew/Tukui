582ccd5a36671b479a96fcbd137831dc08fa88af
Jonathan Bohbot <supra89t@gmail.com>
2026-08-26 14:59:49 -0400

    fix(retail): defer player aura replacement on WoW 12.1

--- Tukui/Modules/Auras/Auras.lua
+++ Tukui/Modules/Auras/Auras.lua
@@ -14,7 +14,17 @@ Auras.Headers = {}
 Auras.FlashTimer = 30
 
 function Auras:DisableBlizzardAuras()
-	BuffFrame:SetParent(T.Hider)
+	-- WoW 12.1 removed SecureAuraHeaderTemplate and introduced AuraContainer.
+	-- Keep Blizzard's player aura containers intact until Tukui's aura module is
+	-- ported to the new Retail API; hiding them here would leave the player with
+	-- no buff/debuff display at all.
+	if T.Midnight then
+		return
+	end
+
+	if BuffFrame then
+		BuffFrame:SetParent(T.Hider)
+	end
 	
 	if DebuffFrame then
 		DebuffFrame:SetParent(T.Hider)
@@ -100,224 +110,35 @@ function Auras:OnUpdate(elapsed)
 end
 
 function Auras:UpdateAura(index)
-	--local Name, Texture, Count, DType, Duration, ExpirationTime, Caster, IsStealable, ShouldConsolidate, SpellID, CanApplyAura, IsBossDebuff
-	
-	--if T.Retail then
-		--local AuraTable = UnitAura(self:GetParent():GetAttribute("unit"), index, self.Filter)
+	local Name, Icon, Count, DebuffType, Duration, ExpirationTime, Caster, CanStealOrPurge, NameplateShowPersonal, SpellID, CanApplyAura, IsBossDebuff, CastByPlayer, NameplateShowAll, TimeMod, Value1, Value2, Value3 = UnitAura("player", index, self.Filter)
+	local Button = self
 
-		--Name = AuraTable.name
-		--Duration = AuraTable.duration
-		--ExpirationTime = AuraTable.expirationTime
-		--Caster = AuraTable.sourceUnit
-		--Texture = AuraTable.icon
-	--else
-		local Name, Texture, Count, DType, Duration, ExpirationTime, Caster, IsStealable, ShouldConsolidate, SpellID, CanApplyAura, IsBossDebuff = UnitAura(self:GetParent():GetAttribute("unit"), index, self.Filter)
-	--end
-
-  --[[    
- spellId=108366,
-  isBossAura=false,
-  duration=0,
-  expirationTime=0,
-  isFromPlayerOrPlayerPet=true,
-  points={
-    [1]=603832
-  },
-  icon=571320,
-  nameplateShowPersonal=false,
-  nameplateShowAll=false,      
- auraInstanceID=272343,
-  timeMod=1,
-  isRaid=false,
-  isHarmful=false,
-  canApplyAura=false,
-  name="Soul Leech",
-  isHelpful=true,
-  applications=0,
-  isNameplateOnly=false,
-  sourceUnit="player",
-  isStealable=false
-}        
-   --]]     
-	if (Name) then
-		if (C.Auras.ClassicTimer) then
-			self.Holder:Hide()
-		else
-			self.Duration:Hide()
-		end
-
-		if (Duration and Duration > 0 and ExpirationTime) then
-			local TimeLeft = ExpirationTime - GetTime()
-
-			if (not self.TimeLeft) then
-				self.TimeLeft = TimeLeft
-				self:SetScript("OnUpdate", Auras.OnUpdate)
-			else
-				self.TimeLeft = TimeLeft
-			end
-
-			self.Dur = Duration
-
-			if C.Auras.Flash then
-				Auras.StartOrStopFlash(self.Animation, TimeLeft)
-			end
-
-			self.Bar:SetMinMaxValues(0, Duration)
-
-			if (not C.Auras.ClassicTimer) then
-				self.Holder:Show()
-			end
-
-			if (Duration == ceil(TimeLeft)) and (C.Auras.Animation and self.AuraGrowth) then
-				self.AuraGrowth:Play()
-			end
-		else
-			if C.Auras.Flash then
-				self.Animation:Stop()
-			end
-
-			self.TimeLeft = nil
-			self.Dur = nil
-			self.Duration:SetText("")
-			self:SetScript("OnUpdate", nil)
-
-			local min, max = self.Bar:GetMinMaxValues()
-
-			self.Bar:SetValue(max)
-			self.Bar:SetStatusBarColor(0, 0.8, 0)
-
-			if (not C.Auras.ClassicTimer) then
-				self.Holder:Hide()
-			end
-		end
-
-		if (Count and Count > 1) then
-			self.Count:SetText(Count)
-		else
-			self.Count:SetText("")
-		end
-
-		if (self.Filter == "HARMFUL") then
-			local Color = DebuffTypeColor[DType or "none"]
-			self.Backdrop:SetBorderColor(Color.r * 3/5, Color.g * 3/5, Color.b * 3/5)
-			self.Holder.Backdrop:SetBorderColor(Color.r * 3/5, Color.g * 3/5, Color.b * 3/5)
-		end
-
-		self.Icon:SetTexture(Texture)
+	if (not Name) then
+		return
 	end
-end
 
-function Auras:UpdateTempEnchant(slot)
-	local Enchant = (slot == 16 and 2) or 6
-	local Expiration = select(Enchant, GetWeaponEnchantInfo())
-	local Icon = GetInventoryItemTexture("player", slot)
+	Button.Icon:SetTexture(Icon)
+	Button.Count:SetText(Count > 1 and Count or "")
+	Button.SpellID = SpellID
+	Button.Caster = Caster
+	Button.Filter = self.Filter
+	Button.Duration:SetText("")
+	Button.TimeLeft = nil
+	Button.Dur = nil
+	Button:SetScript("OnUpdate", nil)
 
-	if C.Auras.ClassicTimer then
-		self.Holder:Hide()
+	if (Duration and Duration > 0) then
+		Button.TimeLeft = ExpirationTime - GetTime()
+		Button.Dur = Duration
+		Button:SetScript("OnUpdate", Auras.OnUpdate)
+	end
+
+	if (DebuffType and DebuffTypeColor and DebuffTypeColor[DebuffType]) then
+		local Color = DebuffTypeColor[DebuffType]
+		Button.Backdrop:SetBorderColor(Color.r, Color.g, Color.b)
 	else
-		self.Duration:Hide()
+		Button.Backdrop:SetBorderColor(unpack(C.General.BorderColor))
 	end
-
-	if (Expiration) then
-		if not self.Dur then
-			self.Dur = Expiration / 1e3
-		end
-
-		self.Enchant = Enchant
-		self:SetScript("OnUpdate", Auras.OnUpdate)
-	else
-		self.Dur = nil
-		self.Enchant = nil
-		self.TimeLeft = nil
-		self:SetScript("OnUpdate", nil)
-	end
-
-	-- Secure Aura Header Fix: sometime an empty temp enchant is show, which should not! Example, Shaman/Flametongue/Two-Handed
-	if Icon then
-		self:SetAlpha(1)
-		self.Icon:SetTexture(Icon)
-	else
-		self:SetAlpha(0)
-	end
-end
-
-function Auras:OnAttributeChanged(attribute, value)
-	if (attribute == "index") then
-		return Auras.UpdateAura(self, value)
-	elseif(attribute == "target-slot") then
-		return Auras.UpdateTempEnchant(self, value)
-	end
-end
-
-function Auras:Skin()
-	local Font = T.GetFont(C["Auras"].Font)
-
-	self:CreateBackdrop()
-	self:CreateShadow()
-
-	self.Backdrop:SetFrameLevel(self:GetFrameLevel() - 2)
-
-	local Icon = self:CreateTexture(nil, "BORDER")
-	Icon:SetTexCoord(unpack(T.IconCoord))
-	Icon:SetInside()
-
-	local Count = self:CreateFontString(nil, "OVERLAY")
-	Count:SetFontObject(Font)
-	Count:SetPoint("TOP", self, 1, -4)
-
-	local Holder = CreateFrame("Frame", nil, self)
-	Holder:SetSize(self:GetWidth(), 4)
-	Holder:SetPoint("TOP", self, "BOTTOM", 0, -1)
-	Holder:CreateBackdrop("Transparent")
-	Holder:CreateShadow()
-
-	local Bar = CreateFrame("StatusBar", nil, Holder)
-	Bar:SetInside()
-	Bar:SetStatusBarTexture(C.Medias.Blank)
-	Bar:SetStatusBarColor(0, 0.8, 0)
-
-	local Duration = self:CreateFontString(nil, "OVERLAY")
-	Duration:SetFontObject(Font)
-	Duration:SetPoint("BOTTOM", 0, -17)
-
-	if C.Auras.Flash then
-		local Animation = self:CreateAnimationGroup()
-		Animation:SetLooping("BOUNCE")
-
-		local FadeOut = Animation:CreateAnimation("Alpha")
-		FadeOut:SetFromAlpha(1)
-		FadeOut:SetToAlpha(0.5)
-		FadeOut:SetDuration(0.6)
-		FadeOut:SetSmoothing("IN_OUT")
-
-		self.Animation = Animation
-	end
-
-	if (C.Auras.Animation and not self.AuraGrowth) then
-		local AuraGrowth = self:CreateAnimationGroup()
-
-		local Grow = AuraGrowth:CreateAnimation("Scale")
-		Grow:SetOrder(1)
-		Grow:SetDuration(0.2)
-		Grow:SetScale(1.25, 1.25)
-
-		local Shrink = AuraGrowth:CreateAnimation("Scale")
-		Shrink:SetOrder(2)
-		Shrink:SetDuration(0.2)
-		Shrink:SetScale(0.75, 0.75)
-
-		self.AuraGrowth = AuraGrowth
-	end
-
-	self.Duration = Duration
-	self.Bar = Bar
-	self.Holder = Holder
-	self.Filter = self:GetParent():GetAttribute("filter")
-
-	self:SetScript("OnAttributeChanged", Auras.OnAttributeChanged)
-
-	self.Icon = Icon
-	self.Count = Count
 end
 
 function Auras:OnEnterWorld()
@@ -337,9 +158,14 @@ function Auras:LoadVariables() -- to be completed
 	local Headers = Auras.Headers
 	local Buffs = Headers[1]
 	local Debuffs = Headers[2]
+
+	if not Buffs or not Debuffs then
+		return
+	end
+
 	local Position = Buffs:GetPoint()
 
-	if Position:match("LEFT") then
+	if Position and Position:match("LEFT") then
 		Buffs:SetAttribute("xOffset", 35)
 		Buffs:SetAttribute("point", Position)
 		Debuffs:SetAttribute("xOffset", 35)
@@ -349,6 +175,13 @@ end
 
 function Auras:Enable()
 	if not C.Auras.Enable then
+		return
+	end
+
+	-- Retail 12.1 replaced SecureAuraHeaderTemplate with the new AuraContainer
+	-- system. Do not create Tukui's legacy headers yet; leave Blizzard auras
+	-- visible while the Tukui presentation is ported to AuraContainer.
+	if T.Midnight then
 		return
 	end
 
